3492b65bf3169c3146cbc349da66e0e934bc8c1b
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-03-15 07:53:32 -0500

    lets see

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import './App.css'
 import { LoginSocialFacebook } from 'reactjs-social-login'
 import { FacebookLoginButton } from 'react-social-login-buttons'
+import { useState,useEffect } from 'react'
 
 function App() {
   
+  let [userName, setUserName]  = useState(null)
+  let [userPicture, setUserPicture] = useState(null)
+
+  
+
   return (
     <>
       <h1>This is just a test application for an activity</h1>
       <LoginSocialFacebook
         appId='7568321253207635' // Replace with your app ID
         onResolve={(response) => {
+          userName = "Welcome " + response.name;
+          setUserName(userName) 
+          userPicture = response.picture.data.url
+          setUserPicture(userPicture) 
           console.log(response);
         }}
         onReject={(error) => console.log(error)}
       >
         <FacebookLoginButton />
       </LoginSocialFacebook>
+      <div>
+          <h1>{userName}</h1>
+          <img src={userPicture}></img>
+        </div>
     </>
   )
 }
