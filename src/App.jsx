41b1a2381fa466a0e5815418575f9f86a8cf6509
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-03-15 08:07:37 -0500

    aver

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,6 @@ function App() {
         appId='7568321253207635' // Replace with your app ID
         onResolve={(response) => {
           console.log(response);
-          // userName = "Welcome" + response.name;
-          console.log(response.name) 
         }}
         onReject={(error) => console.log(error)}
       >
