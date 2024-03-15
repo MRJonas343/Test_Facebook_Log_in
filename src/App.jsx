a0fabc48f0852095ef9ce32a3e8dfdce3b8d93ff
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-03-15 08:10:04 -0500

    ya no le muevo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './App.css'
 import { LoginSocialFacebook } from 'reactjs-social-login'
 import { FacebookLoginButton } from 'react-social-login-buttons'
-import { useState,useEffect } from 'react'
 
 function App() {
   
-  let [userName, setUserName]  = useState(null)
-  let [userPicture, setUserPicture] = useState(null)
-
-  
-
   return (
     <>
       <h1>This is just a test application for an activity</h1>
@@ -22,10 +16,6 @@ function App() {
       >
         <FacebookLoginButton />
       </LoginSocialFacebook>
-      <div>
-          <h1>{userName}</h1>
-          <img src={userPicture}></img>
-        </div>
     </>
   )
 }
