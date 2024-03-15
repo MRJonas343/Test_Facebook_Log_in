041b507ff0170efc1f013c09e8c23d1c99031cae
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-03-15 07:31:25 -0500

    Fix 2.0

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Facebook from 'react-facebook'
-
-const appId = '7568321253207635'; // Replace with your actual app ID
-
-Facebook.init({
-  appId, // Your Facebook app ID
-  xfbml: true,
-  version: 'v15.0', // Specify the Facebook SDK version
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <FacebookProvider appId={appId} version="v15.0">
-      <App />
-    </FacebookProvider>
+    <App />
   </React.StrictMode>,
 )
